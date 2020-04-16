9024db55bccfe54bf40e347d19b4c1d4aefeafbd
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-04-16 09:53:54 +0530

    Defined add subject button functionality in app.js

--- app.js
+++ app.js
@@ -5,14 +5,42 @@ const newPercent = document.querySelector(".percentage");
 const calcButton = document.querySelector(".calcButton");
 const addButton = document.querySelector(".addButton");
 const list = document.querySelector(".list");
+const delButton = document.querySelector(".del");
 
 addButton.addEventListener("click", addSubject);
 calcButton.addEventListener("click", calculateGPA);
-
+delButton.addEventListener("click", deleteEntry);
 function addSubject(e) {
   e.preventDefault();
+  const entry = document.createElement("div");
+  entry.classList.add("entry");
+
+  const subject = document.createElement("li");
+  subject.innerText = newSub.value;
+  entry.appendChild(subject);
+
+  const credit = document.createElement("li");
+  credit.innerText = newCredit.value;
+  entry.appendChild(credit);
+
+  const percent = document.createElement("li");
+  percent.innerText = newPercent.value;
+  entry.appendChild(percent);
+
+  const del = document.createElement("button");
+  del.innerHTML = '<i class="fas fa-trash"></i>';
+  del.classList.add("button", "del");
+  entry.appendChild(del);
+
+  list.appendChild(entry);
 }
 
 function calculateGPA(e) {
   e.preventDefault();
 }
+
+function deleteEntry(e) {
+  console.log(e);
+}
+
+function saveToLocalStorage() {}
