59dc7c1b457ea660a5b2f73eb212a412d6cf46a0
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-04-16 08:35:34 +0530

    Re-defining basic HTML structure

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 //Select DOM
 const fieldsInput = document.querySelector(".boxi");
 const calcButton = document.querySelector(".calcButton");
+const addButton = document.querySelector(".addButton");
+
+addButton.addEventListener("click", addSubject);
+calcButton.addEventListener("click", calculateGPA);
+
+function addSubject(e) {
+  e.preventDefault();
+}
+
+function calculateGPA(e) {
+  e.preventDefault();
+}
